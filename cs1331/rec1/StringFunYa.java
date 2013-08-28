6f0cd64f36413161ec46656c9902c97facd3554d
Thomas Shields <thomas.shields@gatech.edu>
2013-08-28 10:59:20 -0400

    initial commit messages are so boring

---
+++ cs1331/rec1/StringFunYa.java
@@ -0,0 +1,12 @@
+public class StringFunYay {
+	public static void main(String[] ohStringsAgain) {
+		String greeting = "Hello, ";
+		String suffix = "Not gonna type world way too cliche";
+
+		String together = greeting + suffix;
+
+		System.out.println(together);
+
+		
+	}
+}
