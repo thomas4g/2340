02b7aee8898add646af91dd213e0b11aa4af2aad
infor42 <tylerpnn@gmail.com>
2013-09-02 22:14:58 -0400

    base project and main class

---
+++ src/org/mule/Main.java
@@ -0,0 +1,19 @@
+package org.mule;
+
+/**
+ * 
+ * @author Tyler Pennington
+ * 
+ * Main class
+ *
+ */
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+	
+}
+
