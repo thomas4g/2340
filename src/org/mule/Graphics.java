3a3f9afcb17be678743a0bd82d274776bc85d80f
danielecc2594 <danielecc2594@gmail.com>
2013-10-07 22:10:39 -0400

    Adding new graphics engine

--- src/org/mule/Graphics.java
+++ src/org/mule/Graphics.java
@@ -1,5 +1,7 @@
 package org.mule;
 
+import javafx.stage.Stage;
+
 /**
  * Graphics module
  * @author Thomas Shields
@@ -7,36 +9,14 @@ package org.mule;
  */
 public class Graphics {
 	
-	private Screen screen;
+	private Stage stage;
 	
-	/*
-	 * The current screen is is drawn as a collection of
-	 * Textures, GUI elements, and Sprites
-	 * @param the scene object passed from the SceneHandler
-	 */
-	
-	public void recieveScreen(Screen screen){
-		this.screen=screen;
+	public Graphics(){
+		stage=new Stage();
 	}
 	
-	/*
-	 * The current screen is is drawn as a collection of
-	 * Textures, GUI elements, and Sprites
-	 */
-	
-	private void drawFrame(){
-		//draws GUI
-	}
-	private void drawWorld(){
-		//basically (for Tile t in Map: draw(t))
-	}
-	
-	private void drawEntities(){
-		//similiar to drawWorld() but (for Drawable d in entities: draw(d))
-	}
-	
-	public void draw(){
-		//this method checks based on the SceneType which draw methods to call
+	public Stage getStage(){
+		return stage;
 	}
 	
 }
