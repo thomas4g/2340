618b1e7e1e4b0ecf203ed25a927c00db09b64472
havlblodvarg <halvblodvarg@gmail.com>
2013-10-08 00:07:05 -0400

    added the descriptions :D

--- src/edu/gatech/mule/game/CharacterType.java
+++ src/edu/gatech/mule/game/CharacterType.java
@@ -2,10 +2,16 @@ package edu.gatech.mule.game;
 
 public enum CharacterType {
 	
-	HUMANOID("Humanoid", "descript", 600, "human.gif", "human_head.png"),
-	BONZOID("Bonzoid", "descript", "bonzoid.gif", "bonzoid_head.png"),
-	MECHTRON("Mechtron", "mechy", "mechtron.gif", "mechtron_head.png"),
-	FLAPPER("Flapper", "DESCRIPTION HERE", 1600, "flapper.gif", "flapper.png");
+	HUMANOID("Humanoid", "Though the empress herself is said to be a 
+					humanoid, the rest of her assumed race lives in poverty. 
+					Most humanoids work as farmers, servants, or men-for-hire.", 600, "human.gif", "human_head.png"),
+	BONZOID("Bonzoid", "Despite their bulky size, Bonzoids are a peaceful race. 
+					Many work as tailors, blacksmiths, carpenters, and shopowners.", "bonzoid.gif", "bonzoid_head.png"),
+	MECHTRON("Mechtron", "Mechtrons are machines created to do the will of the empress. 
+					They are stronger, faster, and more alter than any race in the empire. 
+					Players cannot control Mechtrons; selecting one creates an npc.", "mechtron.gif", "mechtron_head.png"),
+	FLAPPER("Flapper", "The aristocracy of the empire is primarily composed of Flappers. 
+					Their race has accumulated vast riches by establishing trade with far-off lands.", 1600, "flapper.gif", "flapper.png");
 	
 	private static final double MONEY = 1000;
 	private static final String IMAGE_PATH = "resources/";
