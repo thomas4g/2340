c13fda22030ac0be49fd82d599f03189635a5d4f
danielecc2594 <danielecc2594@gmail.com>
2013-10-09 04:34:33 -0400

    Gargoyles never traveled there...

---
+++ src/mule/View.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mule;
+
+/**
+ *
+ * @author dengel6
+ */
+public interface View {
+    
+    public void setParentScreen(ViewHandler handler);
+    
+}
