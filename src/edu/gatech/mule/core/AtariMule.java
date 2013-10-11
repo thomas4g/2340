631174f09ee2132e716738ea65c78acb91a95fa3
dongsusanna <dongsusanna@gmail.com>
2013-10-11 12:15:00 -0400

    Javadoced the files in the core directory

--- src/edu/gatech/mule/core/AtariMule.java
+++ src/edu/gatech/mule/core/AtariMule.java
@@ -2,6 +2,13 @@ package edu.gatech.mule.core;
 
 import edu.gatech.mule.fx.FXScreenHandler;
 
+/**
+ * Driver for the Atari game M.U.L.E.
+ * Launches the game engine.
+ * 
+ * @version 1.0
+ *
+ */
 public class AtariMule {
 	public static void main(String... args) {
 		GameEngine game = new GameEngine(new FXScreenHandler());
--- src/edu/gatech/mule/core/GameEngine.java
+++ src/edu/gatech/mule/core/GameEngine.java
@@ -6,11 +6,27 @@ import edu.gatech.mule.game.*;
 import edu.gatech.mule.screen.*;
 import edu.gatech.mule.screen.ScreenHandler.ScreenType;
 
+/**
+ * Game engine sets up and runs the MULE game.
+ * 
+ * Sets up the screen handler and game settings.
+ * Has the ability to change the screen view.
+ * 
+ * @version 1.0
+ *
+ */
 public class GameEngine {
 	
 	private ScreenHandler screenHandler;
 	private Settings settings;
 	
+	/**
+	 * Sets up the screen handler and instantiates the settings.
+	 * Also loads the main screen by default.
+	 * 
+	 * @param screenHandler, the screen handler of the program.
+	 * 
+	 */
 	public GameEngine(ScreenHandler screenHandler) {
 		this.screenHandler = screenHandler;
 		this.settings = new Settings();
@@ -19,23 +35,44 @@ public class GameEngine {
 		screenHandler.start();
 	}
 	
+	/**
+	 * Shows the main screen
+	 */
 	public void start() {
 		screenHandler.setScreen(ScreenType.START);
 	}
+	
+	/**
+	 * Shows the config settings
+	 */
 	public void chooseSettings() {
 		screenHandler.setScreen(ScreenType.SETTINGS);
 	}
+	
+	/**
+	 * Shows the screen for choosing race
+	 */
 	public void chooseRace(){
 		screenHandler.setScreen(ScreenType.RACE_SELECT);
 	}
+	
+	/**
+	 * Shows the screen for ???
+	 */
 	public void choosePlayer(){
 		screenHandler.setScreen(ScreenType.PLAYER_SCREEN);
 	}
 	
+	/**
+	 * Shows the screen of the main map ???
+	 */
 	public void playGame() {
 		screenHandler.setScreen(ScreenType.GAME_SCREEN);
 	}
 	
+	/**
+	 * Gets the settings ???
+	 */
 	public Settings getSettings() {
 		return settings;
 	}
