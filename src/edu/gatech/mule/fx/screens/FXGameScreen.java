0e5b7af4c98329e33c8c926a38bb8502b7ff4cb2
Thomas Shields <me@thomasshields.net>
2013-10-20 23:34:54 -0400

    movement yoseph

--- src/edu/gatech/mule/fx/screens/FXGameScreen.java
+++ src/edu/gatech/mule/fx/screens/FXGameScreen.java
@@ -3,9 +3,12 @@ package edu.gatech.mule.fx.screens;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import edu.gatech.mule.core.GameEngine;
 import edu.gatech.mule.fx.FXScreen;
 import edu.gatech.mule.fx.graphics.FXGraphics;
@@ -36,9 +39,24 @@ public class FXGameScreen extends AbstractGameScreen implements Initializable, F
 	public void load() {
 		mapCanvas = new Canvas(720, 400);
 		graphics = new FXGraphics(mapCanvas.getGraphicsContext2D());
-		renderMap();
+		wireKeyboard();
+		render();
 	}
-
+	
+	private void wireKeyboard(){
+		mapCanvas.setFocusTraversable(true);
+		mapCanvas.requestFocus();
+		mapCanvas.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent k) {
+				if(k.getCode().isArrowKey()) {
+					move(
+							k.getCode() == KeyCode.LEFT ? -1 : k.getCode() == KeyCode.RIGHT ? 1 : 0,
+							k.getCode() == KeyCode.DOWN ? -1 : k.getCode() == KeyCode.UP ? 1 : 0);
+				}
+			}
+		});
+	}
 
 	/**
 	 * 
@@ -80,10 +98,11 @@ public class FXGameScreen extends AbstractGameScreen implements Initializable, F
 
 
 	@Override
-	public void renderMap() {
+	public void render() {
 		if(null == mapRenderer)
 			mapRenderer = new OrthogonalMapRenderer(game.getGameMap());
-		mapRenderer.render(graphics);		
+		mapRenderer.render(graphics);	
+		
 	}
 
 }
--- src/edu/gatech/mule/screen/screens/AbstractGameScreen.java
+++ src/edu/gatech/mule/screen/screens/AbstractGameScreen.java
@@ -1,6 +1,9 @@
 package edu.gatech.mule.screen.screens;
 
+import java.util.List;
+
 import edu.gatech.mule.core.GameEngine;
+import edu.gatech.mule.game.Player;
 import edu.gatech.mule.game.Settings;
 import edu.gatech.mule.screen.IScreen;
 
@@ -8,17 +11,30 @@ public abstract class AbstractGameScreen implements IScreen {
 
 	protected GameEngine game;
 	protected Settings settings;
+	private List<Player> players;
+	private Player currentPlayer;
 	
 	public AbstractGameScreen(GameEngine game){
 		this.game = game;
 		this.settings = game.getSettings();
 	}
 	
-	public void done() {
-		
+	public final void done() {
+		//do stuff
 	}
 	
-	public abstract void renderMap();
+	public final void move(int x, int y) {
+		if(null == players) {
+			players = settings.getPlayers();
+			currentPlayer = players.get(0); 
+		}
+		x = x == 0 ? 0 : x/x;
+		y = y == 0 ? 0 : y/y;
+		currentPlayer.move(25*x, 25*y);
+		System.out.println("moving player");
+	}
+	
+	public abstract void render();
 	//TODO add all the map stuff here later
 	
 }
