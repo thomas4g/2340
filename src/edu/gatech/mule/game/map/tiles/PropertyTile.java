3b0114a7b073e4114cb36e16a83e15c9be9823ca
Thomas Shields <tshields@roundarchisobar.com>
2013-11-06 12:56:48 -0500

    basic mule placement/losing

--- src/edu/gatech/mule/game/map/tiles/PropertyTile.java
+++ src/edu/gatech/mule/game/map/tiles/PropertyTile.java
@@ -1,15 +1,22 @@
 package edu.gatech.mule.game.map.tiles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tiled.core.Tile;
+import edu.gatech.mule.game.Mule;
 import edu.gatech.mule.game.Player;
 import edu.gatech.mule.game.map.GameTile;
 import edu.gatech.mule.game.map.TileType;
+import edu.gatech.mule.game.resources.ResourceType;
 
 /**
  * Representation of a property tile
  * @version 0.1
  */
 public class PropertyTile extends GameTile {
+	
+	List<Mule> mules;
 	
 	/**
 	 * Constructor for a property tile
@@ -18,6 +25,14 @@ public class PropertyTile extends GameTile {
 	 */
 	public PropertyTile(Tile t, TileType type) {
 		super(t, type);
+		mules = new ArrayList<Mule>();
+	}
+	
+	public void addMule(Mule mule) {
+		mules.add(mule);
+	}
+	public List<Mule> getMules() {
+		return mules;
 	}
 
 	@Override
--- src/edu/gatech/mule/screen/screens/controllers/GameplayController.java
+++ src/edu/gatech/mule/screen/screens/controllers/GameplayController.java
@@ -8,6 +8,7 @@ import edu.gatech.mule.game.Mule;
 import edu.gatech.mule.game.Player;
 import edu.gatech.mule.game.map.GameTile;
 import edu.gatech.mule.game.map.TileType;
+import edu.gatech.mule.game.map.tiles.PropertyTile;
 import edu.gatech.mule.screen.ScreenHandler.ScreenType;
 import edu.gatech.mule.screen.screens.views.MapView;
 
@@ -43,13 +44,14 @@ public class GameplayController extends MapController {
 		GameTile tile=currentPlayer.getTile();
 		
 		if (currentPlayer.getMule()!=null) {
-			if (tile.hasOwner() && tile.getOwner() == currentPlayer) {
-				System.out.println("your tile");
-			} else {
-				//Make the mule run
-				System.out.println("not your tile");
-			}
-		}else{
+			if (tile.hasOwner() && tile.getOwner() == currentPlayer && tile instanceof PropertyTile) {
+				((PropertyTile)tile).addMule(currentPlayer.getMule());
+			} 
+			
+			entities.remove(currentPlayer.getMule());
+			currentPlayer.setMule(null);
+		} 
+		else{
 			System.out.println("tasteless fools!");
 		}
 		
