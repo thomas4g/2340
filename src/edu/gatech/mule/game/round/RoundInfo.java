e8ebc3aa7f6b99a5f4106c1fb66d15c395e308b7
danielecc2594 <danielecc2594@gmail.com>
2013-11-20 02:13:12 -0500

    Checked Round package

--- src/edu/gatech/mule/game/round/RoundInfo.java
+++ src/edu/gatech/mule/game/round/RoundInfo.java
@@ -1,7 +1,14 @@
 package edu.gatech.mule.game.round;
 
+/**
+ * Holds the data for each of the specific rounds.
+ * @author dengel6
+ *
+ */
+
 public enum RoundInfo {
 
+	//ROUNDX (food requirement, gambling bonus)
 	ROUND1 (3, 50),
 	ROUND2 (3, 50),
 	ROUND3 (3, 50),
@@ -14,19 +21,27 @@ public enum RoundInfo {
 	ROUND10 (5, 150),
 	ROUND11 (5, 150),
 	ROUND12 (5, 200);
-	
 	private int foodReq;
 	private int gambleBonus;
-	
+
 	private RoundInfo(int foodReq, int gambleBonus) {
 		this.foodReq = foodReq;
 		this.gambleBonus = gambleBonus;
 	}
-	
+
+	/**
+	 * Return required food.
+	 * @return required food
+	 */
+
 	public int getFoodReq() {
 		return this.foodReq;
 	}
-	
+
+	/**
+	 * Returns bonus from gambling.
+	 * @return gamble bonus
+	 */
 	public int getGambleBonus() {
 		return this.gambleBonus;
 	}
