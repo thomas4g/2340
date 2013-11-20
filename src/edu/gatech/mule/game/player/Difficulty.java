44d937130fb5ce135840239d3a7cf811555470de
Thomas Shields <tshields@roundarchisobar.com>
2013-11-20 15:06:24 -0500

    difficulty fix for stores

--- src/edu/gatech/mule/game/player/Difficulty.java
+++ src/edu/gatech/mule/game/player/Difficulty.java
@@ -7,9 +7,9 @@ package edu.gatech.mule.game.player;
  */
 public enum Difficulty {
 
-	BEGINNER (new int[] {8, 4, 0, 0}, new int[] {16, 16, 0, 0}),
-	STANDARD (new int[] {4, 2, 0, 0}, new int[] {8, 8, 8, 0}),
-	ADVANCED (new int[] {4, 2, 0, 0}, new int[] {8, 8, 8, 0});
+	BEGINNER (new int[] {8, 4, 0, 0}, new int[] {16, 16, 0, 0, 25}),
+	STANDARD (new int[] {4, 2, 0, 0}, new int[] {8, 8, 8, 0, 14}),
+	ADVANCED (new int[] {4, 2, 0, 0}, new int[] {8, 8, 8, 0, 14});
 
 	private int[] playerResources, storeResources;
 
