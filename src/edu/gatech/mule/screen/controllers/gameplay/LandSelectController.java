dfeb1e1604632fadb5abfc7d7ae757ba37004c86
Thomas Shields <tshields@roundarchisobar.com>
2013-11-20 00:51:07 -0500

    cleaned & commented controllers.gameplay

--- src/edu/gatech/mule/screen/controllers/gameplay/LandSelectController.java
+++ src/edu/gatech/mule/screen/controllers/gameplay/LandSelectController.java
@@ -11,57 +11,56 @@ import edu.gatech.mule.screen.controllers.ScreenController;
 import edu.gatech.mule.screen.views.MapView;
 
 /**
- * Controller for land selection
+ * Controller for land selection.
  * @version 0.1
  */
 public class LandSelectController extends ScreenController {
-	private final static int FREE_ROUNDS = 2;
-	
+	private static final int FREE_ROUNDS = 2;
 	private Point location;
-	
 	private MapView view;
 	private GameMap map;
-	
 	private int currentPlayer;
 	private int round = 1;
-	private boolean skipped = false;
-	
+	private boolean skipped;
+
 	/**
-	 * Constructor for land selection controller
-	 * @param game, game engine
-	 * @param view, map view
+	 * Constructor for land selection controller.
+	 * @param game game engine
+	 * @param view map view
 	 */
 	public LandSelectController(GameEngine game, MapView view) {
-		super(game,view);
+		super(game, view);
 		this.view = view;
-		this.location = new Point(0,0);
+		this.location = new Point(0, 0);
 		this.round = 1;
 	}
-	
+
 	@Override
 	public void load() {
 		super.load();
 		map = game.getGameMap();
+		// empty because land select needs to clear the view's entities
+		// but it doesn't actually use entities itself
 		view.setGameEntities(new ArrayList<Entity>());
 		view.setGameMap(map);
 		setPlayer();
 		view.setPlayers(game.getSettings().getPlayers());
 	}
-	
+
 	@Override
 	public final void move(int x, int y) {
-		x = x == 0 ? 0 : x / Math.abs(x);
-		y = y == 0 ? 0 : y / Math.abs(y);		
-		x = location.x + x >= game.getGameMap().getTiles().length ? 0 : x;
-		y = location.y + y >= game.getGameMap().getTiles()[0].length ? 0 : y;
-		x = location.x + x < 0 ? 0 : x;
-		y = location.y + y < 0 ? 0 : y;
+		x = (x == 0) ? 0 : x / Math.abs(x);
+		y = (y == 0) ? 0 : y / Math.abs(y);
+		x = (location.x + x >= map.getWidth()) ? 0 : x;
+		y = (location.y + y >= map.getHeight()) ? 0 : y;
+		x = (location.x + x < 0) ? 0 : x;
+		y = (location.y + y < 0) ? 0 : y;
 		location.translate(x, y);
 		view.setSelector(location);
 	}
 
 	@Override
-	public void action(){	
+	public void action() {
 		GameTile tile = map.getTile(location.x, location.y);
 		if(tile.getOwner() == null) {
 			if(game.getPlayers().get(currentPlayer).addLand(tile, round <= FREE_ROUNDS)) {
@@ -70,7 +69,7 @@ public class LandSelectController extends ScreenController {
 			}
 		}
 	}
-	
+
 	private void nextPlayer() {
 		currentPlayer++;
 		if(currentPlayer >= game.getPlayers().size()) {
@@ -80,15 +79,14 @@ public class LandSelectController extends ScreenController {
 				if(skipped) {
 					view.setSelector(null);
 					game.getRound().turn();
-				}
-				else {
+				} else {
 					skipped = false;
 				}
 			}
 		}
 		setPlayer();
 	}
-	
+
 	private void setPlayer() {
 		view.setCurrentPlayer(game.getPlayers().get(currentPlayer));
 	}
--- src/edu/gatech/mule/screen/controllers/gameplay/TownController.java
+++ src/edu/gatech/mule/screen/controllers/gameplay/TownController.java
@@ -17,97 +17,107 @@ import edu.gatech.mule.screen.ScreenHandler.ScreenType;
 import edu.gatech.mule.screen.views.TownMapView;
 
 /**
- * Controller for town map
+ * Controller for town map.
  * @version 0.1
  */
 public class TownController extends MapController {
-	
-	public final static int MOVEMENT = 10;
+
+	public static final int MOVEMENT = 10;
 	private TownMapView view;
-	private Store store;	
-	
+	private Store store;
+
 	/**
-	 * Constructor for town controller
-	 * @param game, game engine
-	 * @param view, map view
+	 * Constructor for town controller.
+	 * @param game game engine
+	 * @param view map view
 	 */
-	public TownController(GameEngine game, TownMapView view){
+	public TownController(GameEngine game, TownMapView view) {
 		super(game, view, MOVEMENT);
 		this.view = view;
 	}
-	
+
 	@Override
+	//@TODO Change this logic so that it's being handled from within the tiles
 	public void move(int x, int y) {
 		super.move(x, y);
-		if(
-			(currentPlayer.getTileType() == TileType.EXITTOWN_LEFT && currentPlayer.getDirection() == Direction.LEFT)
+		if((currentPlayer.getTileType() == TileType.EXITTOWN_LEFT
+			&& currentPlayer.getDirection() == Direction.LEFT)
 			||
-			(currentPlayer.getTileType() == TileType.EXITTOWN_RIGHT && currentPlayer.getDirection() == Direction.RIGHT)) {
+			(currentPlayer.getTileType() == TileType.EXITTOWN_RIGHT
+			&& currentPlayer.getDirection() == Direction.RIGHT)) {
 			game.setScreen(ScreenType.GAME_SCREEN);
-		}
-		else if(currentPlayer.getTileType() == TileType.PUB){
+		} else if(currentPlayer.getTileType() == TileType.PUB) {
 			done();
-		}
-		else if(currentPlayer.getTileType() == TileType.RESOURCE_STORE) {
+		} else if(currentPlayer.getTileType() == TileType.RESOURCE_STORE) {
 			view.displayStoreMenu();
-		}
-		else if(currentPlayer.getTileType() == TileType.MULE_STORE && !currentPlayer.hasMule()){
+		} else if(currentPlayer.getTileType() == TileType.MULE_STORE && !currentPlayer.hasMule()) {
 			view.displayMuleOptions();
 		}
 	}
-	
+
 	@Override
 	public void load() {
 		//this override
 		view.setController(this);
 		this.map = game.getTownMap();
 		super.load();
-		
+
 		currentPlayer.useBigSprites(true);
-		
-		
+
 		Point p = new Point(0, 0);
 		switch(currentPlayer.getDirection()) {
 		case DOWN:
-			p.x = (int) map.getWidth()*map.getTileWidth()/2;
+			p.x = (int) map.getWidth() * map.getTileWidth() / 2;
 			p.y = 0;
 			break;
 		case LEFT:
-			p.y = (int) map.getHeight()*map.getTileHeight()/2;
-			p.x = (int) map.getWidth()*map.getTileWidth();
+			p.y = (int) map.getHeight() * map.getTileHeight() / 2;
+			p.x = (int) map.getWidth() * map.getTileWidth();
 			break;
 		case RIGHT:
-			p.y = (int) map.getHeight()*map.getTileHeight()/2;
+			p.y = (int) map.getHeight() * map.getTileHeight() / 2;
 			p.x = 0;
 			break;
 		case UP:
-			p.y = (int) map.getHeight()*map.getTileHeight();
-			p.x = (int) map.getWidth()*map.getTileWidth()/2;
+			p.y = (int) map.getHeight() * map.getTileHeight();
+			p.x = (int) map.getWidth() * map.getTileWidth() / 2;
 			break;
 		default:
 			break;
 		}
 		currentPlayer.setPosition(p);
-		
+
 		if(store == null) {
-			store = new Store(game.getSettings().getDifficulty().getStoreResources(), new int[] {1, 2, 3, 4, 5});
+			//@TODO Law of Demeter violation much?!
+			store = new Store(game.getSettings().getDifficulty().getStoreResources(),
+					new int[] {1, 2, 3, 4, 5});
 		}
-		
-		List<ResourceType> muleTypes = new ArrayList<ResourceType>(Arrays.asList(ResourceType.values()));
+
+		List<ResourceType> muleTypes = new ArrayList<ResourceType>(
+				Arrays.asList(ResourceType.values()));
+
 		muleTypes.remove(ResourceType.MULE);
 		view.setMuleOptions(muleTypes.toArray(new ResourceType[]{}));
 		view.setStoreResourceAmounts(store.getResources());
 		view.setPlayers(game.getSettings().getPlayers());
 	}
-	
+
 	@Override
 	public void done() {
 		turn.done();
 	}
 
+	/**
+	 * Called when a transaction has been completed in GUI.
+	 * @param count the number of things being bought or sold
+	 * @param buying whether or not they're buying or selling
+	 */
 	public void storeComplete(int count, boolean buying) {
 		int[] rDeltas = new int[5];
-		ResourceType type = ResourceType.valueOf(currentPlayer.getTile().getProperties().getProperty("resource_type").toUpperCase());
+
+		//@TODO Again with the Law of Demeter violations!
+		ResourceType type = ResourceType.valueOf(
+				currentPlayer.getTile().getProperties().getProperty("resource_type").toUpperCase());
 		rDeltas[type.ordinal()] = count;
 		Transaction transaction = new Transaction(rDeltas, store.getPrices());
 		if(buying) {
@@ -117,14 +127,20 @@ public class TownController extends MapController {
 		}
 	}
 
+	/**
+	 * Called when the player has selected a mule type in the GUI.
+	 * @param type the type of mule
+	 */
 	public void setMuleType(ResourceType type) {
-		if(currentPlayer.getMule() != null) return;
-		
+		if(currentPlayer.getMule() != null) {
+			return;
+		}
+
 		mule = new Mule(currentPlayer, CharacterType.MULE);
 		mule.setType(type);
 		this.entities.add(mule);
 		currentPlayer.setMule(mule);
 		mule.useBigSprites(true);
-		
+
 	}
 }
