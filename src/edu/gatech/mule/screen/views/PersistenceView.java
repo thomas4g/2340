691f8273618fa6689c01338e39756093a2c283eb
IceShirok <dongsusanna@gmail.com>
2013-11-20 00:20:13 -0500

    javadoced interfacesa nd 1 music file wotow

--- src/edu/gatech/mule/screen/views/PersistenceView.java
+++ src/edu/gatech/mule/screen/views/PersistenceView.java
@@ -2,6 +2,16 @@ package edu.gatech.mule.screen.views;
 
 import edu.gatech.mule.screen.controllers.main.PersistenceController;
 
+/**
+ * Set up for persistence view.
+ * @version 1.0
+ */
 public interface PersistenceView extends SettingsView {
-	public void setController(PersistenceController c);
+
+	/**
+	 * Sets the persistence controller.
+	 * @param c the persistence controller
+	 */
+	void setController(PersistenceController c);
+
 }
